feff8b8355a0502ca7fcc32815d93e6a6aa176f9
Victor Gambarini <vmar559@UoA.auckland.ac.nz>
2022-08-18 19:03:45 +1200

    Added navbar to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,21 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { NavBar } from './components/NavBar';
-import { TaskManager } from './components/TaskManager';
+import { Routes, Route} from "react-router-dom";
+import { Home } from './components/Home';
+import { Members } from './components/Members';
+import { Books } from './components/Books';
 
 function App() {
 
   return (
     <div className="App list-group-item justify-content-center mx-auto" id="App">
       <NavBar />
-      <TaskManager />
-      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/members" element={<Members />} />
+        <Route path="/books" element={<Books />} />
+      </Routes>
     </div>
   );
 }
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import './NavBar.css';
 import logo from '../icon.ico';
+import {Link} from 'react-router-dom';
 
 export const NavBar = () => {
   return (
-    <div className='nav-bar'>
-        <ul className='nav-bar-list'>
-            <li key="Logo">
-                <img className='logo-img' src={logo}></img>
-                <span className='logo-title'>Awesome Library</span>
-            </li>
-            <li key="Home">Home</li>
-            <li key="Members">Members</li>
-            <li key="Books">Books</li>
-        </ul>
-    </div>
+    <nav className='nav-bar'>
+        <Link to="/">
+          <img className='logo-img' src={logo}></img>
+          <span className='logo-title'>Awesome Library</span>
+        </Link>
+      <ul>
+        <Link to="/">
+          <li key="Home">Home</li>
+        </Link>
+        <Link to="/members">
+          <li key="Members">Members</li>
+        </Link>
+        <Link to="/books">
+          <li key="Books">Books</li>
+        </Link>
+      </ul>
+    </nav>
   )
 };
